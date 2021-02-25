4d170b70d7e2abac27ee0d55140e215612c3ffbb
stefan.doyscher@virotechdiagnostics.com <sdoyschman>
2021-02-25 23:05:40 +0100

    added a pipeline object; implement a function, that can read in shader code files.

--- StartPoint/src/first_app.hpp
+++ StartPoint/src/first_app.hpp
@@ -1,11 +1,13 @@
 #pragma once
 
 #include "lve_window.hpp"
+#include "lve_pipeline.hpp"
 
 namespace lve {
   class FirstApp {
   private:
     LveWindow appWindow{ WIDTH, HEIGHT, "Hello Vulkan!" };
+    LvePipeline lvePipeline{"shaders/simple_shader.vert.spv", "shaders/simple_shader.frag.spv"};
   
   public:
     static constexpr int HEIGHT = 600;
